fb410975e0bfc2db77ebbc83404b518eee0e05eb
Eslam Elkholy <eslamelkholy5325762@gmail.com>
2020-09-26 21:00:14 +0200

    Setup Socket IO Connection and Disconnect Functionality

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const socketIO = require("socket.io");
 const http = require("http");
+const router = require("./router");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+
+io.on("connection", (socket) => {
+  console.log("We Have a New Connection!!!");
+  socket.on("disconnect", () => {
+    console.log("User Had Left!!");
+  });
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server Has Started On ${PORT}`));
