d3f5043e3183121c4a1da7cb13ff0d8d0ba5f254
Eslam Elkholy <eslamelkholy5325762@gmail.com>
2020-09-27 11:21:22 +0200

    Added Two Reusable Component InfoBar & Input With It's Styling

--- client/src/Components/Chat/Chat.js
+++ client/src/Components/Chat/Chat.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
 import "./Chat.css";
+import InfoBar from "../Common/InfoBar/InfoBar";
 
 let socket;
 const Chat = ({ location }) => {
@@ -38,11 +39,7 @@ const Chat = ({ location }) => {
   return (
     <div className="outerContainer">
       <div className="container">
-        <input
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          onKeyPress={(e) => (e.key === "Enter" ? sendMessage(e) : null)}
-        />
+        <InfoBar room={room} />
       </div>
     </div>
   );
